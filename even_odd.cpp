5b1bb17062a108469687330d7e107dbdae8007d0
varunvermaa <155096244+varunvermaa@users.noreply.github.com>
2024-02-08 01:46:31 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int a;
+cout << "Enter a number that you want to check even or odd: ";
+cin >> a;
+if(a%2==0){
+cout << a << " is an even number.";
+} 
+else{
+cout << a << " is an odd number.";}
+return 0;
+}
